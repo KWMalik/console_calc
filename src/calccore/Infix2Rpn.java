98100d3ce91e32496eb23e76d79ee31ffc2a8e2b
a.afanasev <Alexander.Afanasev@waveaccess.ru>
2012-08-10 17:20:44 +0400

    infix2rpn class

--- src/calccore/Infix2Rpn.java
+++ src/calccore/Infix2Rpn.java
@@ -3,10 +3,28 @@
  */
 package calccore;
 
+import java.util.*;
+
 /**
  * @author alexandr afanasev
  *
  */
+
+
 public class Infix2Rpn {
 	
+  public static String convert2Rpn(String expr) {
+    //cycle for every char iterating
+    StringBuilder result;
+    Stack <Character> temp;
+    for (int i=0;i<expr.length();i++) {
+      char a=expr.charAt(i);
+      
+    }
+    
+    return null;
+    
+  }
+  
+
 }
